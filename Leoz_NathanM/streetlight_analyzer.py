054d9a928d940805e3988188d9be2897b6a96db5
Leo Zhao <zhao0213@agongquinlive.com>
2023-03-20 12:20:25 -0400

    Finish analyzer.py

---
+++ Leoz_NathanM/streetlight_analyzer.py
@@ -0,0 +1,53 @@
+import arcpy
+import sys
+
+# def main():
+ws = r'..\..\..\..\data\Ottawa'
+arcpy.env.workspace = ws
+streetlight_fc= 'Street_Lights\Street_Lights.shp'
+roads_cl_fc='Road_Centrelines\Road_Centrelines.shp'
+road_name_field=arcpy.AddFieldDelimiters(ws,'ROAD_NAME')
+
+def _get_unique_values(field_name):
+
+   
+    with arcpy.da.SearchCursor(streetlight_fc,'STREET_NAM') as cursor:
+        field_list = []
+        for row in cursor:
+            field_list += row
+        if field_name not in field_list:
+
+            return print('Road name is invalid')
+			break
+        else:
+            return print('Road name is valid')
+			continue
+
+def get_streetlight_count(road_name, distance):
+	def _get_unique_values(road_name)
+    road_segments = arcpy.management.SelectLayerByAttribute(roads_cl_fc, "NEW_SELECTION", f"{road_name_field} = '{road_name}'")
+  
+    near_streetlights,in_lyr,count = arcpy.management.SelectLayerByLocation(streetlight_fc, "WITHIN_A_DISTANCE", road_segments, distance)
+
+    return count
+
+def save_streetlights(road_name, distance, out_fc):
+    road_segments = arcpy.management.SelectLayerByAttribute(roads_cl_fc, "NEW_SELECTION", f"{road_name_field} = '{road_name}'")
+  
+    near_streetlights,in_lyr,count = arcpy.management.SelectLayerByLocation(streetlight_fc, "WITHIN_A_DISTANCE", road_segments, distance)
+   
+    arcpy.management.CopyFeatures(near_streetlights, out_fc)
+
+def show_road_names(pattern=None):
+    if pattern:
+        pattern_upper = pattern.upper()
+        query = f"{road_name_field} LIKE '%{pattern_upper}%'"
+        road_names = _get_unique_values(roads_cl_fc, road_name_field)
+        road_names = [road_name for road_name in road_names if road_name.upper().find(pattern_upper) >= 0]
+    else:
+        query = None
+        road_names = _get_unique_values(roads_cl_fc, road_name_field)
+    print("\n".join(sorted(road_names)))
+
+# if __name__ == "__main__":
+#     main()
---
+++ Leoz_NathanM/streetlight_analyzer_test.py
@@ -0,0 +1,2 @@
+import streetlight_analyzer as sa
+def test_get_unique_values(fc,field_name):
