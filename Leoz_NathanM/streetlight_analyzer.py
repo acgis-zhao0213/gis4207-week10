e9624956b483bce2ec20eb8ffe88cefa4a04149e
acgis-zhao0213 <122320204+acgis-zhao0213@users.noreply.github.com>
2023-03-23 16:58:59 -0400

    Update streetlight_analyzer.py
    updated on March 23. All tests passed.

--- Leoz_NathanM/streetlight_analyzer.py
+++ Leoz_NathanM/streetlight_analyzer.py
@@ -1,51 +1,33 @@
 import arcpy
 import sys
 
-# def main():
 ws = r'..\..\..\..\data\Ottawa'
 arcpy.env.workspace = ws
 streetlight_fc= 'Street_Lights\Street_Lights.shp'
-roads_cl_fc='Road_Centrelines\Road_Centrelines.shp'
-road_name_field=arcpy.AddFieldDelimiters(ws,'ROAD_NAME')
+roads_cl_fc = 'Road_Centrelines\Road_Centrelines.shp'
+road_name_field = 'ROAD_NAME'
 
 def _get_unique_values(fc, field_name):
       unique_values = set()
-      with arcpy.da.SearchCursor(fc,[field_name]) as cursor:
+      with arcpy.da.SearchCursor(fc,field_name) as cursor:
         for row in cursor: 
            unique_values.add(row[0])
         return unique_values 
 
 def get_streetlight_count(road_name, distance):
-    
-    streetlight = "Street_Lights"
-
-    unique_roads = _get_unique_values("Road_Centrelines", "ROAD_NAME")
-
-    if road_name in unique_roads: 
-        arcpy.management.SelectLayerByLocation("Road_Centrelines", "NEW_SELECTION", "ROADNAME = '{}'".format(road_name), None) 
-        arcpy.management.SaveToLayerFile('Road_Centrelines', road_name) 
-
-
-        Selection_Layer = arcpy.management.SelectLayerByLocation(streetlight, 'WITHIN_A_DISTANCE', road_name + '.lryx', distance, "NEW_SELECTION", "NOT_INVERT")
-        return arcpy.management.GetCount(Selection_Layer)[0]
-    else: 
+    unique_roads = _get_unique_values(roads_cl_fc, "ROAD_NAME")
+    if road_name in unique_roads:
+        road_segments = arcpy.management.SelectLayerByAttribute(roads_cl_fc, "NEW_SELECTION", f"{road_name_field} = '{road_name}'")
+        near_streetlights,in_lyr,count = arcpy.management.SelectLayerByLocation(streetlight_fc, "WITHIN_A_DISTANCE", road_segments, distance)
+    else:
         print('Road Invalid')
-        print (unique_roads)
-        pass 
-
-Road = "CRAMER" 
-road_distance = "0.002 DecimalDegrees"
-
-get_streetlight_count(Road, road_distance)
-
-
-
+        pass
+    return count
+# print(get_streetlight_count('BRADLEY',0.0002))
 
 def save_streetlights(road_name, distance, out_fc):
     road_segments = arcpy.management.SelectLayerByAttribute(roads_cl_fc, "NEW_SELECTION", f"{road_name_field} = '{road_name}'")
-  
     near_streetlights,in_lyr,count = arcpy.management.SelectLayerByLocation(streetlight_fc, "WITHIN_A_DISTANCE", road_segments, distance)
-   
     arcpy.management.CopyFeatures(near_streetlights, out_fc)
 
 def show_road_names(pattern=None):
@@ -58,6 +40,4 @@ def show_road_names(pattern=None):
         query = None
         road_names = _get_unique_values(roads_cl_fc, road_name_field)
     print("\n".join(sorted(road_names)))
-
-# if __name__ == "__main__":
-#     main()
+    return road_names
