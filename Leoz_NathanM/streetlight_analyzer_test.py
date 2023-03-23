9173bed68ffa0b574dc4ec48d0eef477b6fca2d3
acgis-zhao0213 <122320204+acgis-zhao0213@users.noreply.github.com>
2023-03-23 17:00:12 -0400

    Update streetlight_analyzer_test.py
    All tests passed.

--- Leoz_NathanM/streetlight_analyzer_test.py
+++ Leoz_NathanM/streetlight_analyzer_test.py
@@ -1,16 +1,34 @@
 import streetlight_analyzer as sa
 import arcpy
-import sys
+import sys,os
 
 ws = r'..\..\..\..\data\Ottawa'
 arcpy.env.workspace = ws
 streetlight_fc= 'Street_Lights\Street_Lights.shp'
 roads_cl_fc='Road_Centrelines\Road_Centrelines.shp'
-road_name_field=arcpy.AddFieldDelimiters(ws,'ROAD_NAME')
+road_name_field=arcpy.AddFieldDelimiters(ws,'ROAD_NAME_')
+def test_get_unique_values():
+    test_name = 'BRADLEY'
+    uniques = sa._get_unique_values(roads_cl_fc, "ROAD_NAME")
+    assert test_name in uniques
+
 def test_get_streetlight_count():
     expect=19
-    act=sa.get_streetlight_count('BRADLEY',0.0002 )
+    act=int(sa.get_streetlight_count('BRADLEY',0.0002 ))
     assert expect == act
 
-test_get_streetlight_count()
+def test_save_streetlights():
+    test_output = 'test.shp'
+    sa.save_streetlights('BRADLEY',0.0002, 'test')
+    filelist = []
+    for root,dirs,files in os.walk(ws):
+        for f in files:
+            if f.find('.shp')>=0:
+                filelist.append(f)
+    assert test_output in filelist
 
+def test_show_road_names():
+    names = sa.show_road_names(pattern=None)
+    test_output = 'ZOKOL'
+    print('names type',type(names))
+    assert test_output in names
